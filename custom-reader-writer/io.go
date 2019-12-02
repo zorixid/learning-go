7c762f83e415d17df19c182ab782fe602f893bd7
zorixid <elijah.dufour@outlook.com>
2019-12-01 21:20:25 -0800

    custom reader writer

---
+++ custom-reader-writer/io.go
@@ -0,0 +1,59 @@
+// This code defines two custom types: FooReader & FooWriter
+// On each type we define an implementation of the Read([]byte) or Write([]byte) function
+// These functions either read or write from stdin/stdout
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// Custom type FooReader defines an io.Reader to read from stdin
+type FooReader struct{}
+
+// Read reads data from stdin
+func (fooReader *FooReader) Read(b []byte) (int, error) {
+	fmt.Print("in > ")
+	return os.Stdin.Read(b)
+}
+
+// Custom type FooWriter defines an io.Writer to write to Stdout
+type FooWriter struct{}
+
+// Write writes data to Stdout
+func (fooWriter *FooWriter) Write(b []byte) (int, error) {
+	fmt.Print("out> ")
+	return os.Stdout.Write(b)
+}
+
+func main() {
+	// Instantiate reader and writer
+	var (
+		reader FooReader
+		writer FooWriter
+	)
+
+	// Everything below does the same thing that `io.Copy()` does.
+	// The code can be simplified as follows:
+	// if _, err := io.Copy(&writer, &reader); err != nil {
+	// 	log.Fatalln("Unable to read/write data")
+	// }
+
+	// Create buffer to hold input/output
+	input := make([]byte, 4096)
+
+	// Use reader to read input
+	s, err := reader.Read(input)
+	if err != nil {
+		log.Fatalln("Unable to read data")
+	}
+	fmt.Printf("Read %d bytes from stdin\n", s)
+
+	// Use writer to write output
+	s, err = writer.Write(input)
+	if err != nil {
+		log.Fatalln("Unable to write data")
+	}
+	fmt.Printf("Wrote %d bytes to stdout\n", s)
+}
